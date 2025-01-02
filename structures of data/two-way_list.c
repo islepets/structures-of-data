84c89121d3f8abf0d3086124159d0a00c006626c
islepets <slepecila@gmail.com>
2025-01-02 23:14:48 +0400

    Добавлена функция init()

--- structures of data/two-way_list.c
+++ structures of data/two-way_list.c
@@ -4,6 +4,22 @@
 #include <stdlib.h>
 
 
+struct list {
+	int field;
+	struct list* next;
+	struct list* prev;
+};
+
+struct list* init(int a) {
+	struct list* lst = (struct list*)malloc(sizeof(struct list));
+
+	lst->field = a;
+	lst->next = NULL;
+	lst->prev = NULL;
+
+	return lst;
+}
+
 int main() {
 
 	return 0;
