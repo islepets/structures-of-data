49af4424706e347975ea15d2a6f83f4376b2fd5b
islepets <slepecila@gmail.com>
2025-01-02 23:22:49 +0400

    Добавлены функции add() и delete()

--- structures of data/two-way_list.c
+++ structures of data/two-way_list.c
@@ -20,6 +20,40 @@ struct list* init(int a) {
 	return lst;
 }
 
+struct list* add(struct list* lst, int number) {
+	struct list* temp, * p;
+	temp = (struct list*)malloc(sizeof(struct list));
+
+	p = lst->next;
+	lst->next = temp;
+
+	temp->field = number;
+	temp->next = p;
+	temp->prev = lst;
+
+	if (p != NULL) {
+		p->prev = temp;
+	}
+
+	return temp;
+}
+
+struct list* delete(struct list* lst) {
+	struct list* next, * prev;
+	next = lst->next;
+	prev = lst->prev;
+
+	if (prev != NULL) {
+		prev->next = lst->next;
+	}
+	if (next != NULL) {
+		next->prev = lst->prev;
+	}
+	free(lst);
+
+	return prev;
+}
+
 int main() {
 
 	return 0;
