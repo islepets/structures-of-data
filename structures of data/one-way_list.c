e3b7da497efe84ed06f789b5c2d2276086bae05b
islepets <slepecila@gmail.com>
2025-01-02 22:44:23 +0400

    Добавлены структуры

--- structures of data/one-way_list.c
+++ structures of data/one-way_list.c
@@ -3,7 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct list_node {
+	struct list_t* next;
+	void* data;
+}list_node_t;
 
+typedef struct list {
+	int size;	
+	list_node_t* head;
+	list_node_t* tail;
+}list_t;
 
 int main() {
 
