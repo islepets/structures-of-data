2117e36035e8589b88131fb8be15e44fc5647cc9
islepets <slepecila@gmail.com>
2025-01-02 23:04:13 +0400

    Добавление функций push() и pop()

--- structures of data/one-way_list.c
+++ structures of data/one-way_list.c
@@ -14,6 +14,52 @@ typedef struct list {
 	list_node_t* tail;
 }list_t;
 
+list_t* create_list() {
+	list_t* lst = malloc(sizeof(list_t));
+
+	lst->size = 0;
+	lst->head = NULL;
+	lst->tail = lst->head;
+
+	return lst;
+}
+
+void push(list_t* lst, void* data) {
+
+	list_node_t* node = malloc(sizeof(list_node_t));
+	node->data = data;
+
+	if (lst->tail != NULL) {
+		lst->tail->next = node;
+	}
+	else {
+		lst->head = node;
+	}
+
+	lst->tail = node;
+	lst->size++;
+}
+
+void* pop(list_t* lst) {
+	if (lst->size == 0) {
+		return NULL;
+	}
+
+	list_node_t* node = lst->head;
+	void* ret_val = node->data;
+
+	lst->size--;
+	lst->head = node->next;
+
+	free(node);
+
+	if (lst->size == 0) {
+		lst->head = NULL;
+		lst->tail = NULL;
+	}
+	return ret_val;
+}
+
 int main() {
 
 
